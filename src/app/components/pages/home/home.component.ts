5da86ce27d7ebb84db4a0666495b3c01ccbd581e
Jose Castro Cantillo <drakonc@gmail.com>
2019-12-23 20:33:58 -0500

    Primeros Pasos con Matirial

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  public posts: {
+    id: string;
+    titlePost: string;
+    contentPost: string;
+    imagePost: string;
+  }[] = [
+    {
+      id: "1",
+      titlePost: "Post Numero Uno",
+      contentPost:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+      imagePost: "https://i.picsum.photos/id/237/200/300.jpg"
+    },
+    {
+      id: "2",
+      titlePost: "Post Numero Dos",
+      contentPost:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+      imagePost: "https://i.picsum.photos/id/237/200/300.jpg"
+    },
+    {
+      id: "3",
+      titlePost: "Post Numero Tres",
+      contentPost:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+      imagePost: "https://i.picsum.photos/id/237/200/300.jpg"
+    },
+    {
+      id: "4",
+      titlePost: "Post Numero Cuatro",
+      contentPost:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+      imagePost: "https://i.picsum.photos/id/237/200/300.jpg"
+    },
+    {
+      id: "5",
+      titlePost: "Post Numero Cuatro",
+      contentPost:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+      imagePost: "https://i.picsum.photos/id/237/200/300.jpg"
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
