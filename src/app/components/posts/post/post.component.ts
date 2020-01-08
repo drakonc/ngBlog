3442c9f8c784a816d693dc72d91a647623127984
Jose Castro <drakonc@gmail.com>
2020-01-08 18:52:17 -0500

    Añadiendo toolbar

--- src/app/components/posts/post/post.component.ts
+++ src/app/components/posts/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-post',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  public post: { id: string; titlePost: string; contentPost: string; imagePost: string; } = {
+    id: "1",
+    titlePost: "Post Numero Uno",
+    contentPost: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    imagePost: "https://i.picsum.photos/id/237/200/300.jpg"
+
+  };
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.post.id = this.route.snapshot.params.id;
   }
 
 }
